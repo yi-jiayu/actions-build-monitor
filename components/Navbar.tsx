7c4b5e114b13ceba3da8a1f3a68f4b9fac13bfef
Jiayu Yi <yijiayu@gmail.com>
2022-03-30 22:53:48 +0800

    Introduce Navbar and Layout components

---
+++ components/Navbar.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import React from "react";
+import {CogIcon} from "@heroicons/react/outline";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-center bg-white border-b py-4 px-6">
+      <div className="grow max-w-7xl flex justify-between items-center">
+        <div className="flex items-center gap-x-6">
+          <Link href="/">
+            <a className="font-bold">Home</a>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <Link href="/settings">
+          <a>
+            <CogIcon className="h-6 w-6" />
+          </a>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
